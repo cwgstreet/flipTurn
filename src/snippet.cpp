bc18f3b1ada88093ce30cf676860f2cc908a0804
Carl <cwgstreet@gmail.com>
2023-12-04 14:56:19 +1030

    Update snippet.cpp

--- src/snippet.cpp
+++ src/snippet.cpp
@@ -1,2 +1,35 @@
 // snippet to pass Stuct to function to set RGB colours
 
+/*
+old code
+/* ************* end readBattery() **********************************************/
+
+/* define RGB values for LED status colours
+int BLUE_BT_CONNECTED[] = {0, 0, 255};
+int GREEN_FULLY_CHARGED[] = {0, 255, 0};
+int YELLOW_LOW_BATT[] = {255, 255, 0};
+int RED_CRITICALLY_low_BAT[] = {255, 0, 0};
+
+int *status_colour_set[] = {BLUE_BT_CONNECTED, GREEN_FULLY_CHARGED, YELLOW_LOW_BATT, RED_CRITICALLY_low_BAT};  // array of pointers
+*/
+/*****************************************************************************
+    function name : setRGBcolour()
+------------------------
+Description : sets a defined colour on RGB LED by setting R, G and B values in an array
+
+Input Value : R, G and B values for a specific colour output
+                see https://www.w3schools.com/colors/colors_picker.asp
+Return Value: - n/a -
+********************************************************************************/
+void setRGBcolour(int r, int g, int b) {
+    analogWrite(RED_LED_PIN, r);
+    analogWrite(GREEN_LED_PIN, g);
+    analogWrite(BLUE_LED_PIN, b);
+}
+/* ************* end setRGBcolour() **********************************************/
+*/
+
+struct StatusColour {
+// add members
+
+};
